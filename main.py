4bbda18f09539013c1749d02d8f7e0f4c7b7c053
ZorikIordanyan <7.zorik@mail.ru>
2022-07-08 13:23:58 +0300

    added some tasks

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-a = int(input())
-b = int(input())
-if b != 0:
-    print(a / b)
-else:
-    print('Деление невозможно')
-    b = int(input('Введите ненулевое значение '))
-    if b == 0:
-        print('Потрачено')
-    else:
-        print(a / b)
+i = 0
+while i < 5:
+    print('*')
+    if i % 2 == 0:
+        print('**')
+    if i > 2:
+        print('***')
+    i = i + 1
